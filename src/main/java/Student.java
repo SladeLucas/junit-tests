bd241972e9ebcb46ed7c9082da0c2d94ecdbf995
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-05 11:27:20 -0700

    student class has some functionality

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class Student {
+    private String name;
+    private long id;
+    private ArrayList<Integer> grade;
+
+    public Student(String name){
+        this.name=name;
+        this.id=id;
+        this.grade = new ArrayList<>();
+    }
+
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void addGrade(int grade){
+        this.grade.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades(){
+        return this.grade;
+    }
+
+    public double getGradeAverage(){
+        double avg = 0;
+        for (int x : grade){
+            avg += x;
+        }
+        return avg / grade.size();
+    }
 }
